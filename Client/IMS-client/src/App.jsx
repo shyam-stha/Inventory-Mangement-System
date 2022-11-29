29e4566404124d8c9397366bd2b50d3ff988cd38
Shyam Stha <sthask2019@gmail.com>
2022-11-29 23:25:59 +0545

    feat: Update home page with different reuseable component

--- Client/IMS-client/src/App.jsx
+++ Client/IMS-client/src/App.jsx
@@ -1,11 +1,19 @@
-import react from 'react'
+import react from "react";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-   return (
-   <>
-      <p>App</p>
-   </>
-  )
+  return (
+      <BrowserRouter>
+         <Routes>
+            <Route path="/">
+                <Route index element={<Home />} />
+                <Route path="login" element={<Login />} />
+            </Route>
+         </Routes>
+      </BrowserRouter> 
+   );
 }
 
-export default App
+export default App;
