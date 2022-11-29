07a7481ab5acf22a260872430be3788edcaa7e4a
Shyam Stha <sthask2019@gmail.com>
2022-11-29 23:32:10 +0545

    feat : Update home pages with different component

--- Client/IMS-client/src/pages/Home/Home.jsx
+++ Client/IMS-client/src/pages/Home/Home.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import "./home.scss"
+import React from "react";
+import Appbar from "../../Components/Appbar/Appbar";
+import Drawer from "../../Components/Drawer/Drawer";
+import "./home.scss";
 
 const Home = () => {
   return (
-    <div className='home'>
-        Home
+    <div className="home">
+      <div className="sidebar">
+        <Drawer />
+      </div>
+      <div className="container">
+        <Appbar />
+        Container
+        </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home 
+export default Home;
