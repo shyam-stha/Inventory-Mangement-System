da854d30c12b5bbb5c8659052f81d4add56ac626
Shyam Stha <sthask2019@gmail.com>
2022-11-30 15:01:42 +0545

    feat : Update routes

--- Client/IMS-client/src/App.jsx
+++ Client/IMS-client/src/App.jsx
@@ -1,19 +1,21 @@
 import react from "react";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
+import Category from "./Container/Category/Categories/Category";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-      <BrowserRouter>
-         <Routes>
-            <Route path="/">
-                <Route index element={<Home />} />
-                <Route path="login" element={<Login />} />
-            </Route>
-         </Routes>
-      </BrowserRouter> 
-   );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="category"  element={<Category />}/>
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
