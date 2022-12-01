64a82b7dfe386d0eac921d0011f66a1a824f523b
Shyam Stha <sthask2019@gmail.com>
2022-12-01 08:11:29 +0545

    feat: Add middlewares and routes

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,6 +1,24 @@
-import express from 'express'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { connectDB } from "./Database/Database.js";
+import categoryRouter from "./Routes/categoriesApi.js";
 
 const app = express();
 
+try {
+  connectDB();
+} catch (error) {
+  console.log("Database error: ", error);
+}
 
-export default app
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/category", categoryRouter);
+
+app.use(async (err, req, res, next) => {
+  console.log(err);
+});
+
+export default app;
