a64efc7ed1c014235ebff797839feb9f187c8649
Shyam Stha <sthask2019@gmail.com>
2022-12-02 22:06:28 +0545

    feat : Update drawer and styling

--- Client/IMS-client/src/Components/Drawer/Drawer.jsx
+++ Client/IMS-client/src/Components/Drawer/Drawer.jsx
@@ -1,41 +1,93 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./drawer.scss";
 
 const Drawer = () => {
-  return (<div className="drawer">
-    <div className="top">
+  return (
+    <div className="drawer">
+      <div className="top">
         <h3 className="logo">IMS</h3>
-    </div>
-    <div className="center">
+      </div>
+      <div className="center">
         <p className="tittle">Main</p>
         <li className="drawer-item">
-          <img className="icons" src="https://img.icons8.com/color/2x/dashboard-layout.png"  alt="Dashboard" />  Dashboard
+          <Link to="dashboard">
+            <img
+              className="icons"
+              src="https://img.icons8.com/color/2x/dashboard-layout.png"
+              alt="Dashboard"
+            />{" "}
+            Dashboard{" "}
+          </Link>
         </li>
         <p className="tittle">Lists</p>
         <li className="drawer-item">
-          <img className="icons" src="https://cdn-icons-png.flaticon.com/512/747/747545.png"  alt="User" />  Users
+          <Link to="users">
+            <img
+              className="icons"
+              src="https://cdn-icons-png.flaticon.com/512/747/747545.png"
+              alt="User"
+            />{" "}
+            Users{" "}
+          </Link>
         </li>
         <li className="drawer-item">
-          <img className="icons" src="https://cdn-icons-png.flaticon.com/512/2816/2816954.png"  alt="Category" />  Category
+          <Link to="category">
+            <img
+              className="icons"
+              src="https://cdn-icons-png.flaticon.com/512/2816/2816954.png"
+              alt="Category"
+            />{" "}
+            Category{" "}
+          </Link>
         </li>
         <li className="drawer-item">
-          <img className="icons" src="https://cdn-icons-png.flaticon.com/128/9005/9005812.png"  alt="Products" />  Products
+          <Link to="products">
+            <img
+              className="icons"
+              src="https://cdn-icons-png.flaticon.com/128/9005/9005812.png"
+              alt="Products"
+            />{" "}
+            Products{" "}
+          </Link>
         </li>
         <li className="drawer-item">
-          <img className="icons" src="https://cdn-icons-png.flaticon.com/128/5643/5643764.png"  alt="Orders" />  Orders
+          <Link to="orders">
+            <img
+              className="icons"
+              src="https://cdn-icons-png.flaticon.com/128/5643/5643764.png"
+              alt="Orders"
+            />{" "}
+            Orders
+          </Link>
         </li>
         <p className="tittle">Services</p>
         <li className="drawer-item">
-          <img className="icons" src="https://cdn-icons-png.flaticon.com/512/1170/1170616.png"  alt="Reports" />  Reports
+          <Link to="reports">
+            <img
+              className="icons"
+              src="https://cdn-icons-png.flaticon.com/512/1170/1170616.png"
+              alt="Reports"
+            />{" "}
+            Reports
+          </Link>
         </li>
         <li className="drawer-item">
-          <img className="icons" src="https://cdn-icons-png.flaticon.com/512/2965/2965279.png"  alt="Settings" />  Settings
+          <Link to="settings">
+            <img
+              className="icons"
+              src="https://cdn-icons-png.flaticon.com/512/2965/2965279.png"
+              alt="Settings"
+            />{" "}
+            Settings
+          </Link>
         </li>
-    </div>
-    <div className="buttom">
+      </div>
+      <div className="buttom">
         <p>color options</p>
+      </div>
     </div>
-  </div>);
+  );
 };
 
 export default Drawer;
